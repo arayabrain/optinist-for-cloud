c4b9fcc1be98f604b8238e005e108fd36700fec0
Miles <miles_desforges@araya.org>
2024-09-25 18:22:28 +0100

    removing changes to dev files
    removing changes to dev files
    
    removing changes to dev files
    
    removing changes to dev files
    
    removing changes to dev files

--- studio/__main_unit__.py
+++ studio/__main_unit__.py
@@ -187,7 +187,7 @@ async def index(request: Request):
 
 def main(develop_mode: bool = False):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--port", type=int, default=8000)
     parser.add_argument("--reload", action="store_true")
     args = parser.parse_args()
