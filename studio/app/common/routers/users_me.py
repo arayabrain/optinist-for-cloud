c1e7aaa44b84b79c3226428e8a3ff9352d801ffe
tsuchiyama-araya <tsuchiyama_yutaka@araya.org>
2025-05-08 10:28:07 +0900

    cannot delete when running with delete me api

--- studio/app/common/routers/users_me.py
+++ studio/app/common/routers/users_me.py
@@ -3,7 +3,9 @@ from sqlmodel import Session
 
 from studio.app.common.core.auth.auth_dependencies import get_current_user
 from studio.app.common.core.users import crud_users
+from studio.app.common.core.workspace.workspace_services import WorkspaceService
 from studio.app.common.db.database import get_db
+from studio.app.common.models.workspace import Workspace
 from studio.app.common.schemas.users import SelfUserUpdate, User, UserPasswordUpdate
 
 router = APIRouter(prefix="/users/me", tags=["users/me"])
@@ -41,6 +43,22 @@ async def delete_me(
     current_user: User = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    return await crud_users.delete_user(
+    # Fetch workspaces owned by the user
+    workspaces = db.query(Workspace).filter(Workspace.user_id == current_user.id).all()
+    workspace_ids = [ws.id for ws in workspaces]
+
+    # Delete all experiments associated with the user's workspaces
+    for workspace_id in workspace_ids:
+        WorkspaceService.delete_workspace_experiment_by_workspace_id(db, workspace_id)
+
+    # Delete workspace data
+    for workspace_id in workspace_ids:
+        WorkspaceService.delete_workspace_data(workspace_id=workspace_id)
+
+    # Delete the workspace from the database
+    WorkspaceService.delete_workspace_data_by_user_id(db, current_user.id)
+
+    delete_user = await crud_users.delete_user(
         db, current_user.id, organization_id=current_user.organization.id
     )
+    return delete_user
